e09352dfad739f15db68bb8383364d5ef13675d2
Skalman77 <henrik_e@hotmail.com>
2018-01-22 16:36:38 +0100

    Updated scripts

--- VW_CarNet/carnet_windowheat_off.sh
+++ VW_CarNet/carnet_windowheat_off.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/home/pi/volkswagen-carnet/carnet_domoticz.py -t stop-window-heating > /dev/null 2>&1 &
+/home/pi/volkswagen-carnet/carnet_domoticz.py -t stop-window-heating -w > /dev/null 2>&1 &
 exit 0
--- VW_CarNet/carnet_windowheat_on.sh
+++ VW_CarNet/carnet_windowheat_on.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/home/pi/volkswagen-carnet/carnet_domoticz.py -t start-window-heating > /dev/null 2>&1 &
+/home/pi/volkswagen-carnet/carnet_domoticz.py -t start-window-heating -w > /dev/null 2>&1 &
 exit 0
